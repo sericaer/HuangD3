2fac6412d4fc97ad75fddd81552c6c5f94224374
sericaer <sericaer@outlook.com>
2018-10-28 22:54:27 +0800

    init

---
+++ Assets/MainScene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainScene : MonoBehaviour
+{
+    void Awake()
+    {
+        var Toggles = GameObject.Find("Canvas/PanelRight/").GetComponentsInChildren<Toggle>();
+        foreach (var toggle in Toggles)
+        {
+            if(toggle.isOn)
+            {
+                SceneManager.LoadSceneAsync(toggle.name, LoadSceneMode.Additive);
+            }
+
+            toggle.onValueChanged.AddListener((bool isOn)=>{
+                if (!isOn)
+                {
+                    return;
+                }
+
+                for(int i=0; i<SceneManager.sceneCount; i++)
+                {
+                    string sceneName = SceneManager.GetSceneAt(i).name;
+                    if (sceneName != SceneManager.GetActiveScene().name)
+                    {
+                        SceneManager.UnloadSceneAsync(sceneName);
+                    }
+                }
+
+                SceneManager.LoadSceneAsync(toggle.name, LoadSceneMode.Additive);
+                });
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
