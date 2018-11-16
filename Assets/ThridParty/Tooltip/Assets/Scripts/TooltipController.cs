e40c206302ae7701e7eaabf9dbccd76492fd5b59
sericaer <sericaer@outlook.com>
2018-11-16 16:28:42 +0800

    tooltip

---
+++ Assets/ThridParty/Tooltip/Assets/Scripts/TooltipController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TooltipController : MonoBehaviour
+{
+    public static TooltipController Instance;
+
+    public GameObject TooltipPanel;
+    public Text TooltipText;
+    public float PaddingFromMouse;
+
+    private RectTransform _panelRect;
+
+    void Awake()
+    { 
+        Instance = this;
+        _panelRect = TooltipPanel.GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+        transform.position = RefreshPosition();
+    }
+
+    void Update()
+    {
+        transform.position = RefreshPosition();
+    }
+
+    private Vector3 RefreshPosition()
+    {
+        float width = _panelRect.rect.width;
+        float height = _panelRect.rect.height;
+
+        bool flipVertical = false;
+        bool flipHorizontal = false;
+        if (Input.mousePosition.x + width > Screen.width)
+        {
+            flipHorizontal = true;
+        }
+        if (Input.mousePosition.y - height < 0)
+        {
+            flipVertical = true;
+        }
+
+        Vector3 offset = new Vector2();
+        if (!flipHorizontal)
+        {
+            offset.x = PaddingFromMouse;
+        }
+        else
+        {
+            offset.x = -width + -PaddingFromMouse;
+        }
+        if (!flipVertical)
+        {
+            offset.y = -PaddingFromMouse;
+        }
+        else
+        {
+            offset.y = height + PaddingFromMouse;
+        }
+
+        return Input.mousePosition + offset;
+    }
+}
---
+++ Assets/ThridParty/Tooltip/Assets/Scripts/TooltipElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/*
+    Any element that needs to display a tooltip should attach this script. 
+*/
+
+public class TooltipElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string TooltipText;
+    public GameObject tooltip;
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        GameObject UIRoot = GameObject.Find("Canvas").gameObject;
+        tooltip = Instantiate(Resources.Load(string.Format("Prefabs/TooltipCanvas")), UIRoot.transform) as GameObject;
+        tooltip.GetComponent<TooltipController>().TooltipText.text = TooltipText;
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        Destroy(tooltip);
+    }
+}
